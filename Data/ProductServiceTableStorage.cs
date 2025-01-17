bf8527fac7d598894e55cda480f58f1ede0a02aa
Stefan Traub <traub@hs-ulm.de>
2025-01-17 10:46:30 +0100

    shared secret

--- Data/ProductServiceTableStorage.cs
+++ Data/ProductServiceTableStorage.cs
@@ -15,22 +15,22 @@ public class ProductServiceTableStorage : IProductService
 
     public List<Product> GetProducts()
     {
-        // string? cs = configuration["cs"];
-        // if (cs == null)
-        // {
-        //     logger.LogError("Connection string is null");
-        //     return new List<Product>();
-        // }
-        // TableClient cl = new TableClient(cs, "Products");
-        DefaultAzureCredential cred = new DefaultAzureCredential();
-        //AzureCliCredential cred = new AzureCliCredential();
-        string? tableuri = configuration["TableURI"];
-        if (tableuri == null)
+        string? cs = configuration["cs"];
+        if (cs == null)
         {
-            logger.LogError("Table URI is null");
+            logger.LogError("Connection string is null");
             return new List<Product>();
         }
-        TableClient cl = new TableClient(new Uri(tableuri),"Products", cred);
+        TableClient cl = new TableClient(cs, "Products");
+        // DefaultAzureCredential cred = new DefaultAzureCredential();
+        // //AzureCliCredential cred = new AzureCliCredential();
+        // string? tableuri = configuration["TableURI"];
+        // if (tableuri == null)
+        // {
+        //     logger.LogError("Table URI is null");
+        //     return new List<Product>();
+        // }
+        // TableClient cl = new TableClient(new Uri(tableuri),"Products", cred);
         Azure.Pageable<Product> r = cl.Query<Product>();
         return r.ToList();
     }
